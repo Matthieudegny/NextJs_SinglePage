03cdbcf5bbe7cc40e3eb7407e401fe2d63a0820e
Matthieudegny <matthieu.dev.degny@gmail.com>
2023-09-29 19:37:13 +0200

    section 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
 import Image from "next/image";
+import logoFirst from "../public/loader-logo-animated2.png";
 export default function Home() {
   return (
     // { menu sticky not on first section + blue buton sticky}
     <main>
       {/* menu + logo +video */}
-      <section className="sm:h-760 w-full">
+      <section className="h-760 w-full relative bg-blue-400">
         <video autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover z-0">
           <source src={require("../public/teaser_header.mp4")} type="video/mp4" />
-          {/* Vous pouvez également ajouter d'autres formats de vidéo ici */}
         </video>
+        <div className="h-full flex flex-col justify-center items-center">
+          <div className="z-20">
+            <Image src={logoFirst} alt="logo animated first page" width={87} quality={100} />
+          </div>
+          <h1 className="text-white text-21 tracking-wide leading-25 font-montserratSemiBold mt-11.18  z-20">
+            TARAWA
+          </h1>
+          <h2 className="text-white text-7 mt-1.46 leading-9 font-montserratBold tracking-5.6 z-20">HOTEL</h2>
+        </div>
       </section>
       {/* blue title + black title + text + blue buton + buton*/}
       <section></section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,42 @@ const config: Config = {
       height: {
         760: "760px",
       },
+      top: {
+        "50%": "50%",
+      },
+      left: {
+        "50%": "50%",
+      },
+      translate: {
+        "-50%": "-50%",
+      },
+      fontSize: {
+        "7": "7px",
+        "21": "21px",
+      },
+      letterSpacing: {
+        tightest: "-.075em",
+        tighter: "-.05em",
+        tight: "-.025em",
+        normal: "0",
+        wide: ".039375em",
+        wider: ".05em",
+        widest: ".1em",
+        "5.6": "5.6px",
+      },
+      lineHeight: {
+        "9": "9px",
+        "25": "25px", // Définissez votre hauteur de ligne personnalisée
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans"], // Définissez votre fonte Montserrat personnalisée
+        montserratSemiBold: ["Montserrat-SemiBold", "sans"], // Définissez votre fonte Montserrat semi-bold personnalisée
+        montserratBold: ["Montserrat-Bold", "sans"], // Définissez votre fonte Montserrat bold personnalisée
+      },
+      margin: {
+        "1.46": "1.46px",
+        "11.18": "11.18px",
+      },
     },
   },
   plugins: [],
